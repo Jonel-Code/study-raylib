7a3dd17868067b6de1be805a51b65eced6b3c738
Jonel-Code <41978152+Jonel-Code@users.noreply.github.com>
2026-02-15 18:32:43 +0800

    remove unused lines

--- main.cpp
+++ main.cpp
@@ -2,12 +2,7 @@
 #include "vector"
 #include "string"
 #include "raylib.h"
-#include "./src/printer.cpp"
-#include "./src/controllable_pawn.cpp"
 #include "./src/named_colors.cpp"
-#include "./src/level_bound_system.cpp"
-#include "./src/moving_ball.cpp"
-#include "./src/bouncing_ball_system.cpp"
 #include "./src/systems/traversal_system.cpp"
 #include "./src/systems/level_system.cpp"
 
@@ -16,19 +11,6 @@ int main()
     int WindowWidth = 800;
     int WindowHeight = 450;
     InitWindow(WindowWidth, WindowHeight, "Clicker");
-    LevelBoundSystem *levelBoundSystem = LevelBoundSystem::Instance();
-    levelBoundSystem->Init(Vector2{10, 10}, WindowWidth - 20, WindowHeight - 20);
-
-    ControllablePawn *playerPawn = new ControllablePawn(50);
-
-    std::vector<MovingBall *> ballPool = {
-        new MovingBall(
-            Vector2{(float)WindowWidth / 2.f, -(float)WindowHeight / 2.f},
-            Vector2{200, 500}),
-        new MovingBall(
-            Vector2{(float)WindowWidth / 2.f, -(float)WindowHeight / 2.f},
-            Vector2{-100, 100}),
-    };
 
     Vector2 PlayOrigin{10, 10};
     Vector2 PlayEnd{(float)(WindowWidth - 20), (float)(WindowHeight - 20)};
@@ -53,9 +35,6 @@ int main()
         fps = GetFrameTime();
         BeginDrawing();
         ClearBackground(CORNFLOWER_BLUE);
-        levelBoundSystem->Update();
-        // playerPawn->Update();
-        // BatchHandleBouncingBall(ballPool);
         RenderTraversingEntities(Balls);
         UpdateTraversingEntities(Balls);
         ContainTraversingEntities(Balls, BoxLevel);
